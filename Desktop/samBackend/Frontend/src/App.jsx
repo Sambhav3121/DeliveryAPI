bd753b3243e0a0ef4132036118091ade5a8dd9dc
Sambhav3121 <sambhavbajaj31@gmail.com>
2025-04-03 01:27:52 +0700

    Update App.jsx for Dish feature

--- Desktop/samBackend/Frontend/src/App.jsx
+++ Desktop/samBackend/Frontend/src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import UserProfile from './views/UserProfile';
+import DishView from './components/Dish/DishView'; // Import DishView
 
 const App = () => {
     return (
-        <div className="App">
-            <h1>Welcome to the User Profile App</h1>
-            <UserProfile />
-        </div>
+        <Router>
+            <div className="App">
+                <h1>Welcome to the User Profile App</h1>
+                <Routes>
+                    <Route path="/" element={<UserProfile />} />
+                    <Route path="/dishes" element={<DishView />} /> {/* Add DishView route */}
+                </Routes>
+            </div>
+        </Router>
     );
 };
 
